a7fb47a843a31306347cfa4c8443ed7496b2155e
Masa <h.masakataka736@icloud.com>
2017-05-31 15:11:43 +0900

    code

--- Tempature.py
+++ Tempature.py
@@ -1,5 +1,6 @@
 import spidev
 import time
+import os
 import sys
 
 spi = spidev.SpiDev()
@@ -7,17 +8,17 @@ spi.open(0, 0)
 
 def ReadChannel(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    #data = ((adc[1]&3) << 8) + adc[2]
-    return (((adc[1]&3) << 8) + adc[2])
+    data = ((adc[1]&3) << 8) + adc[2]
+    return data
 
-def convertVolts(data):
+def convertVolts(data,places):
     volts = (data * 3.3) / float(1023)
-    volts = round(volts, 4)
+    volts = round(volts, places)
     return volts
 
-def convertTemp(volts):
+def convertTemp(volts,places):
     temp = (100 * volts) - 50.0
-    temp = round(temp, 4)
+    temp = round(temp, places)
     return temp
 
 temp_channel = 0
@@ -26,10 +27,8 @@ delay = 1
 
 while True:
     data = ReadChannel(0)
-    volts = convertVolts(data)
-    temp = convertTemp(volts)
-    print("adc:".format(data))
-    print("volts:".format(volts))
-    print("temp:".format(temp))
+    volts = convertVolts(data,2)
+    temp = convertTemp(data,2)
+    print("Temp:{}({}V{}degC)".format(data,volts,temp))
     print("--------------------")
     time.sleep(1)
