501f09fb57f885241a7b57b56cc7d96c9eab9104
Masa <h.masakataka736@icloud.com>
2017-05-31 17:21:09 +0900

    code

--- Light.py
+++ Light.py
@@ -2,31 +2,32 @@ import spidev
 import time
 import os
 
+
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
 def ReadChannel(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    #data = ((adc[1] & 3) << 8) + adc[2]
-    return ((adc[1] & 3) << 8) + adc[2]
+    return ((adc[1]&3) << 8) + adc[2]
 
-def ConvertVolts(data,places):
+def convertVolts(data,places):
     volts = (data * 3.3) / float(1023)
-    volts = round(volts)
+    volts = round(volts, places)
     return volts
 
-def ConvertTemp(data,places):
-    temp = ((data * 330) / float(1023)) - 50
+def convertTemp(data,places):
+    temp = ((330 * data) / float(1023)) - 50.0
     temp = round(temp, places)
     return temp
 
-light_channel = 0
+temp_channel = 0
 
 delay = 5
 
 while True:
-    light_level = ReadChannel(light_channel)
-    light_volts = ConvertVolts(light_level, 2)
+    light_level = ReadChannel(0)
+    light_volts = convertVolts(light_level, 2)
     print "--------------------------------------------"
     print("Light: {} ({}V)".format(light_level, light_volts))
     time.sleep(delay)
+
