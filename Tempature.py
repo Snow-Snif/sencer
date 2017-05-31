e0378d0d5088e923eb814110872f493249753a1e
Masa <h.masakataka736@icloud.com>
2017-05-31 14:52:20 +0900

    code

--- Tempature.py
+++ Tempature.py
@@ -6,7 +6,7 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 
 def ReadChannel(channel):
-    adc = spi.xfer([1, (8 + channel) << 4, 0])
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
     #data = ((adc[1]&3) << 8) + adc[2]
     return (((adc[1]&3) << 8) + adc[2])
 
