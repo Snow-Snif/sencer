ef8f10c944f5f9a40cd1614c7f97d2f35d37aca4
Masa <h.masakataka736@icloud.com>
2017-05-31 14:48:02 +0900

    code

--- Light.py
+++ Light.py
@@ -7,8 +7,8 @@ spi.open(0, 0)
 
 def ReadChannel(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
+    #data = ((adc[1] & 3) << 8) + adc[2]
+    return ((adc[1] & 3) << 8) + adc[2]
 
 def ConvertVolts(data,places):
     volts = (data * 3.3) / float(1023)
