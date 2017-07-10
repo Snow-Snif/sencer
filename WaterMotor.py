d9f5753d5062784fa0c8320126b2de5bfea4f1ae
Masa <h.masakataka736@icloud.com>
2017-07-10 16:34:26 +0900

    ,km n
    Signed-off-by: Masa <h.masakataka736@icloud.com>

---
+++ WaterMotor.py
@@ -0,0 +1,38 @@
+import wiringpi
+import time
+import sys
+
+motor1_pin = 23
+motor2_pin = 24
+
+param = sys.argv
+
+order = param[1]
+
+second = int(param[2])
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode( motor1_pin, 1 )
+wiringpi.pinMode( motor2_pin, 1 )
+
+if order == "go":
+    if second == 0:
+        print("回転 止めるときはbreak 0コマンド！")
+    else:
+        print(str(second)+"秒回転")
+    wiringpi.digitalWrite( motor1_pin, 1 )
+    wiringpi.digitalWrite( motor2_pin, 0 )
+    time.sleep(second)
+elif order == "back":
+    if second == 0:
+        print("逆回転 止めるときはbreak 0コマンド！")
+    else:
+        print(str(second)+"秒逆回転")
+    wiringpi.digitalWrite( motor1_pin, 0 )
+    wiringpi.digitalWrite( motor2_pin, 1 )
+    time.sleep(second)
+
+if order == "break" or second != 0:
+    print("ブレーキ！")
+    wiringpi.digitalWrite( motor1_pin, 1 )
+    wiringpi.digitalWrite( motor2_pin, 1 )
